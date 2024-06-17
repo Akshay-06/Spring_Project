154f220eb7a7d8afa9c2aa9b4f38dba77d5d345a
Akshay-06 <akshayreddy1999@gmail.com>
2024-06-17 15:50:31 -0400

    Added for understanding the concept of Dependency Injection

--- src/main/java/com/spring/SpringProjectApplication.java
+++ src/main/java/com/spring/SpringProjectApplication.java
@@ -2,12 +2,19 @@ package com.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringProjectApplication.class, args);
+		ApplicationContext  context = SpringApplication.run(SpringProjectApplication.class, args);
+		
+		
+		// Usually we create the object by saying new Class() and we need to maintain that class
+		// But in Spring, we inject the object of that class,  this concept is known as Dependency Injection
+		Alien alien = context.getBean(Alien.class);
+		alien.code();
 	}
 
 }
