dce2aab1f498c4b00059793ac6af39ae625574b4
Akshay-06 <akshayreddy1999@gmail.com>
2024-06-17 15:21:55 -0400

    Added for GET Api which will fetch data from DB using Spring Boot, JPA

---
+++ src/main/java/com/spring/User.java
@@ -0,0 +1,54 @@
+package com.spring;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Users")
+public class User {
+
+	@Id
+	private int id;
+	private String username;
+	private String email;
+	private String password;
+	private int age;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", email=" + email + ", password=" + password + ", age="
+				+ age + "]";
+	}
+	
+	
+}
---
+++ src/main/java/com/spring/UserResource.java
@@ -0,0 +1,21 @@
+package com.spring;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserResource {
+
+	
+	@Autowired
+	UserRepository repo;
+	
+	@GetMapping("getUsers")
+	public List<User> getAllUsers(){
+		return (List<User>) repo.findAll();
+	}
+	
+}
